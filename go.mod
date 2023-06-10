6f09fed5b073acdc93b9bbd357d49f95b4772e34
Raihanpoke <raihanmalay14@gmail.com>
2023-06-10 14:32:29 +0700

    menambah dependency golang

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Raihanpoke/app-say-hello
+
+go 1.20
+
+require github.com/Raihanpoke/go-say-hello v0.0.0-20230610071552-741bdb990b98 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/Raihanpoke/go-say-hello"
+)
+
+func main() {
+	result := go_say_hello.SayHello()
+	fmt.Println(result)
+}
